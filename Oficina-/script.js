3e3da9294c74c5a969b3fcac59b8e586916d2f7a
ThaiDelgado <thaissadelgado@gmail.com>
2020-09-12 15:09:39 -0300

    Arquivos adicionados

--- Oficina-/script.js
+++ Oficina-/script.js
@@ -1,1 +1,13 @@
-console.log ("entrei")
+let titulo = document.querySelector("h2")
+let input = document.querySelector("input")
+let botao = document.querySelector("button")
+console.log("titulo, input, botao")
+
+function cliquei() {
+    
+    event.preventDefault()
+    titulo.innerHTML = input.value
+
+
+
+}
